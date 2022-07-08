6e82dd655ee9bb2ebc06bc50c9df8087ba2fd9ad
umavanth21 <56533761+umavanth21@users.noreply.github.com>
2022-07-08 23:12:11 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,12 @@
 output "bucket_name" {
   value = aws_s3_bucket.hcmx-s3.id
 }
+
+output "bucket_domain_name" {
+  value = aws_s3_bucket.hcmx-s3.bucket_domain_name
+}
+
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.hcmx-s3.bucket_regional_domain_name
+}
+
